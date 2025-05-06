c23a30a8082d9361b72a0d6efe887d8256365fa9
Sufahaniya <sufahaniya@gmail.com>
2025-05-06 20:44:33 +0530

    day-2 learning

--- demo.py
+++ demo.py
@@ -18,3 +18,12 @@ signup_btn =WebDriverWait(driver,10).until(
 signup_btn.click()
 time.sleep(3)
 print("signup button clicked successfully!")
+
+# step 3 : 
+driver.find_elements(By.ID,"user_name").send_keys("haniya")
+driver.find_elements(By.ID,"password").send_keys("12345")
+
+signup_btn_submit=WebDriverWait(driver,10).until(
+    EC.element_to_be_clickable((By.XPATH,"234"))
+)
+signup_btn_submit.click()
